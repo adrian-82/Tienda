d4c12c05d8aafacb2ef9e2ce8ae07f242aa448c0
Cristian <cristianj503@gmail.com>
2020-01-21 17:27:53 +0100

    Linea= Producto + Cantida ensayo 1

---
+++ Linea.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+/**
+ *programa que guarda producto + cantidad.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Linea
+{
+    public static void main(String[] args)
+    {
+       Producto producto = new Producto();
+        HashMap<String,Integer> de = new HashMap<>();
+       // String  producto;
+        int cantidad;
+   // de.put("chicles" , 14);
+   // de.put("Piruletas", 100);
+   //de.put("Gomitas" , 50);
+   //de.put("Palomitas", 50);
+   
+   //de.put(producto.getnombre(), producto.getprecio();
+   
+   System.out.println("Productos en la tienda "+de);
+    }
+    
+}
